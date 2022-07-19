fb5a3ea83c0ff1ae2a3769a92c073523538a9ed9
Unal <unaltugrulbulut@gmail.com>
2022-07-19 11:27:26 +0300

    DeckModulePagePage name changed

--- src/test/java/net/seamlessly/pages/DeckModulePage.java
+++ src/test/java/net/seamlessly/pages/DeckModulePage.java
@@ -3,8 +3,8 @@ package net.seamlessly.pages;
 import net.seamlessly.utilities.Driver;
 import org.openqa.selenium.support.PageFactory;
 
-public class DeckPage extends BasePage{
-    public DeckPage(){
+public class DeckModulePage extends BasePage{
+    public DeckModulePage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
 }
