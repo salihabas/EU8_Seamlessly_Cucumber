fa7b6c4469d611c22d3a0660506a790e5aff1ba9
Unal <unaltugrulbulut@gmail.com>
2022-07-19 23:16:17 +0300

    TC004 elements and methods done

--- src/test/java/net/seamlessly/pages/DeckModulePage.java
+++ src/test/java/net/seamlessly/pages/DeckModulePage.java
@@ -1,6 +1,7 @@
 package net.seamlessly.pages;
 
 import com.github.javafaker.Faker;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import net.seamlessly.utilities.BrowserUtils;
 import net.seamlessly.utilities.Driver;
 import org.junit.Assert;
@@ -13,6 +14,65 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DeckModulePage extends BasePage{
+
+    private String newBoardName;
+    private String newListName;
+
+    int indexNumberOfListOfCurrentBoard = 0;
+    private String newCardName;
+
+    int anyThreeDotsIndexNumber = 0;
+
+    private int countOfAvatarsOnCards;
+
+    private int afterAssignCountofAvatarsOnCards;
+
+
+    public int getAfterAssignCountofAvatarsOnCards() {
+        return afterAssignCountofAvatarsOnCards;
+    }
+
+    public void setAfterAssignCountofAvatarsOnCards() {
+        List<WebElement> listOfAvatarsOnCards = Driver.getDriver().findElements(By.xpath("//div[@class='avatars']//div[@class='avatardiv popovermenu-wrapper has-tooltip']"));
+        afterAssignCountofAvatarsOnCards = listOfAvatarsOnCards.size();
+
+    }
+
+    public int getCountOfAvatarsOnCards() {
+        return countOfAvatarsOnCards;
+    }
+
+    public void setCountOfAvatarsOnCards() {
+        List<WebElement> listOfAvatarsOnCards = Driver.getDriver().findElements(By.xpath("//div[@class='avatars']//div[@class='avatardiv popovermenu-wrapper has-tooltip']"));
+        countOfAvatarsOnCards=listOfAvatarsOnCards.size();
+    }
+
+    public String getNewCardName() {
+        return newCardName;
+    }
+
+    public void setNewCardName() {
+        Faker faker = new Faker();
+        newCardName = faker.animal().name();
+    }
+
+    public String getNewBoardName() {
+        return newBoardName;
+    }
+
+    public void setNewBoardName() {
+        Faker faker = new Faker();
+        newBoardName = faker.funnyName().name();
+    }
+
+    public String getNewListName() {
+        return newListName;
+    }
+
+    public void setNewListName() {
+        Faker faker = new Faker();
+        newListName = faker.idNumber().valid();
+    }
     public DeckModulePage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
@@ -38,6 +98,26 @@ public class DeckModulePage extends BasePage{
     @FindBy(xpath = "(//input[@id='new-stack-input-main'])[1]")
     public WebElement enterListNameInputBox;
 
+    @FindBy(xpath = "//input[@id='new-stack-input-main']")
+    public WebElement cardNameInput;
+
+    @FindBy(xpath = "(//div[@class='open']//li//button)[1]")
+    public WebElement assignToMe;
+
+
+
+
+    public WebElement findBoardFromBoards(String boardName){
+        List<WebElement> boardList = Driver.getDriver().findElements(By.xpath("//ul[@class='app-navigation-entry__children']//span[@title]"));
+        WebElement foundElement = null;
+        for (WebElement eachBoard : boardList) {
+            if (eachBoard.getAttribute("title").equals(boardName)){
+                foundElement = eachBoard;
+            }
+        }
+        return foundElement;
+    }
+
     public void checkcreatedBoard (String boardName){
 
         List<WebElement> boardList = Driver.getDriver().findElements(By.xpath("//ul[@class='app-navigation-entry__children']//span[@title]"));
@@ -52,10 +132,10 @@ public class DeckModulePage extends BasePage{
 
     }
 
-    public void clickfirstBoard(){
-
+    public void clickAnyBoard(){
+        Faker faker = new Faker();
         List<WebElement> boardList = Driver.getDriver().findElements(By.xpath("//ul[@class='app-navigation-entry__children']//span[@title]"));
-        boardList.get(0).click();
+        boardList.get(faker.number().numberBetween(0,boardList.size()-1)).click();
     }
 
 
@@ -69,5 +149,45 @@ public class DeckModulePage extends BasePage{
         }
         Assert.assertTrue(count>0);
     }
+
+
+
+    public void clickAnyAddButtonOnTheCurrentBoard(){
+        Faker faker = new Faker();
+        List<WebElement> addIcons = Driver.getDriver().findElements(By.xpath("//div[@class='board']//button[@icon]"));
+        this.indexNumberOfListOfCurrentBoard= faker.number().numberBetween(0,addIcons.size()-1);
+        addIcons.get(indexNumberOfListOfCurrentBoard).click();
+    }
+
+    public WebElement findCardAddedList(){
+        List<WebElement> allLists = Driver.getDriver().findElements(By.xpath("//div[@class='stack']"));
+        return allLists.get(indexNumberOfListOfCurrentBoard);
+    }
+
+    public String getAddedCardText(){
+        List<WebElement> cards = Driver.getDriver().findElements(By.xpath("(//div[@class='stack'])[" + indexNumberOfListOfCurrentBoard + "]//div//h3//span"));
+        return cards.get(cards.size()-1).getText();
+    }
+
+    public void clickAnyThreeDotsOnCards(){
+        Faker faker = new Faker();
+        List<WebElement> threeDotsList = Driver.getDriver().findElements(By.xpath("//div[@class='smooth-dnd-container vertical']//div//button[@aria-label='Actions']"));
+        anyThreeDotsIndexNumber = faker.number().numberBetween(0,threeDotsList.size()-1);
+        BrowserUtils.sleep(2);
+        threeDotsList.get(anyThreeDotsIndexNumber).click();
+        if (Driver.getDriver().findElement(By.xpath("(//div[@tabindex='0']//span[@class='action-button__text'])[1]")).getText().equals("Assign to me")){
+            return;
+        }else {
+            Driver.getDriver().navigate().refresh();
+            clickAnyThreeDotsOnCards();
+        }
+    }
+
+
+
+
+
+
+
 
 }
