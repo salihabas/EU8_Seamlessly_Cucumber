811e9cc7849fcdd0927c5138878ef73510a833e8
Unal <unaltugrulbulut@gmail.com>
2022-07-24 22:38:54 +0300

    DeckModulePage updating

--- src/test/java/net/seamlessly/pages/DeckModulePage.java
+++ src/test/java/net/seamlessly/pages/DeckModulePage.java
@@ -7,6 +7,7 @@ import net.seamlessly.utilities.BrowserUtils;
 import net.seamlessly.utilities.Driver;
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -15,67 +16,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DeckModulePage extends BasePage {
+    public final String newBoardName ="Board1";
+    public final String board_2_Name = "Board2";
 
-    private String newBoardName;
-    private String newListName;
-
-    int indexNumberOfListOfCurrentBoard = 0;
-    private String newCardName;
-
-    int anyThreeDotsIndexNumber = 0;
-
-    private int countOfAvatarsOnCards;
-
-    private int afterAssignCountofAvatarsOnCards;
-
-    DeckModule_StepDefs deckModule_stepDefs;
-
-
-    public int getAfterAssignCountofAvatarsOnCards() {
-        return afterAssignCountofAvatarsOnCards;
-    }
-
-    public void setAfterAssignCountofAvatarsOnCards() {
-        List<WebElement> listOfAvatarsOnCards = Driver.getDriver().findElements(By.xpath("//div[@class='avatars']//div[@class='avatardiv popovermenu-wrapper has-tooltip']"));
-        afterAssignCountofAvatarsOnCards = listOfAvatarsOnCards.size();
-
-    }
-
-    public int getCountOfAvatarsOnCards() {
-        return countOfAvatarsOnCards;
-    }
-
-    public void setCountOfAvatarsOnCards() {
-        List<WebElement> listOfAvatarsOnCards = Driver.getDriver().findElements(By.xpath("//div[@class='avatars']//div[@class='avatardiv popovermenu-wrapper has-tooltip']"));
-        countOfAvatarsOnCards = listOfAvatarsOnCards.size();
-    }
-
-    public String getNewCardName() {
-        return newCardName;
-    }
-
-    public void setNewCardName() {
-        Faker faker = new Faker();
-        newCardName = faker.animal().name();
-    }
-
-    public String getNewBoardName() {
-        return newBoardName;
-    }
-
-    public void setNewBoardName() {
-        Faker faker = new Faker();
-        newBoardName = faker.funnyName().name();
-    }
-
-    public String getNewListName() {
-        return newListName;
-    }
-
-    public void setNewListName() {
-        Faker faker = new Faker();
-        newListName = faker.idNumber().valid();
-    }
+    public final String newListName="List1";
+    public final String list_2_Name ="List2";
+    public final String newCardName = "Card1";
 
     public DeckModulePage() {
         PageFactory.initElements(Driver.getDriver(), this);
@@ -93,8 +39,6 @@ public class DeckModulePage extends BasePage {
     @FindBy(xpath = "//input[@class='icon-confirm']")
     public WebElement confirmIcon;
 
-    @FindBy(xpath = "//ul[@class='app-navigation-entry__children']//span[@title]")
-    public WebElement referanceOfBoardList;
 
     @FindBy(xpath = "//button[@icon='icon-add']")
     public WebElement addListIcon;
@@ -102,32 +46,35 @@ public class DeckModulePage extends BasePage {
     @FindBy(xpath = "//div[@id='stack-add']//input[@id='new-stack-input-main']")
     public WebElement enterListNameInputBox;
 
+    @FindBy(xpath = "//div[@class='stack']//button[@icon='icon-add']")
+    public WebElement addCardButton;
     @FindBy(xpath = "//input[@id='new-stack-input-main']")
     public WebElement cardNameInput;
 
     @FindBy(xpath = "(//div[@class='open']//li//button)[1]")
     public WebElement assignToMe;
 
-    @FindBy(xpath = "//div[@class='board']//button[@icon]")
-    public WebElement addCardButtons;
+    @FindBy(xpath = "//div[@class='avatar-list']//img")
+    public WebElement assignedAvatar;
+
 
     @FindBy(xpath = "//div[@class='smooth-dnd-container vertical']//div//button[@aria-label='Actions']")
     public WebElement threeDotsOnCurrentBoard;
 
-    @FindBy(xpath = "//span[.='Assign to me']")
-    public WebElement assignToMeText;
+    @FindBy(xpath = "(//div[@class='open']//li)[2]//button")
+    public WebElement moveCardOnDropdown;
+
+    @FindBy(xpath = "(//ul[@class='multiselect__content']//li)[2]")
+    public WebElement board2ToSelect;
 
 
-    public WebElement findBoardFromBoards(String boardName) {
-        List<WebElement> boardList = Driver.getDriver().findElements(By.xpath("//ul[@class='app-navigation-entry__children']//span[@title]"));
-        WebElement foundElement = null;
-        for (WebElement eachBoard : boardList) {
-            if (eachBoard.getAttribute("title").equals(boardName)) {
-                foundElement = eachBoard;
-            }
-        }
-        return foundElement;
-    }
+    @FindBy(xpath = "//span[@class='action-button__icon icon-delete']/..")
+    public WebElement deleteBoard;
+
+    @FindBy(xpath = "//button[@class='error primary']")
+    public WebElement deleteBoardConfirm;
+
+
 
     public void checkcreatedBoard() {
         WebElement board = Driver.getDriver().findElement(By.xpath("//span[@title='" + newBoardName + "']") );
@@ -135,29 +82,22 @@ public class DeckModulePage extends BasePage {
         Assert.assertTrue(board.isDisplayed());
     }
 
-    public void clickAnyBoard() {
-        Faker faker = new Faker();
-        List<WebElement> boardList = Driver.getDriver().findElements(By.xpath("//ul[@class='app-navigation-entry__children']//span[@title]"));
-        boardList.get(faker.number().numberBetween(0, boardList.size() - 1)).click();
+
+
+    public void clickAnyCreatedBoard() {
+        WebElement createdBoard = Driver.getDriver().findElement(By.xpath("//span[@title='" + newBoardName + "']/.."));
+        createdBoard.click();
     }
 
 
     public void checkCreatedListName() {
 
-        WebElement createdList = Driver.getDriver().findElement(By.xpath("//h3[contains(text(),'" + newListName + "')]"));
+        WebElement createdList = Driver.getDriver().findElement(By.xpath("//span[contains(text(),'" + newBoardName + "')]"));
         Assert.assertTrue(createdList.isDisplayed());
     }
 
-
     public void clickAnyAddCardButtonOnTheCurrentBoard() {
-        if (!addCardButtons.isDisplayed()){
-            deckModule_stepDefs.user_clicks_add_list_button_top_right_of_the_page();
-            deckModule_stepDefs.user_enters_any_list_name_and_presses_enter_key();
-        }
-        Faker faker = new Faker();
-        List<WebElement> addIcons = Driver.getDriver().findElements(By.xpath("//div[@class='board']//button[@icon]"));
-        indexNumberOfListOfCurrentBoard = faker.number().numberBetween(0, addIcons.size() - 1);
-        addIcons.get(indexNumberOfListOfCurrentBoard).click();
+        addCardButton.click();
     }
 
     public void  checkCreatedCard() {
@@ -165,24 +105,18 @@ public class DeckModulePage extends BasePage {
         Assert.assertTrue(createdCard.isDisplayed());
     }
 
-
-
     public void clickAnyThreeDotsOnCards() {
-        Faker faker = new Faker();
-        if (!threeDotsOnCurrentBoard.isDisplayed()){
-            deckModule_stepDefs.user_clicks_add_list_button_top_right_of_the_page();
-            deckModule_stepDefs.user_enters_any_list_name_and_presses_enter_key();
-            deckModule_stepDefs.user_clicks_add_card_button_of_any_created_list();
-            deckModule_stepDefs.user_enters_new_card_name_and_presses_enter();
-            clickAnyThreeDotsOnCards();
-        }
+        threeDotsOnCurrentBoard.click();
+    }
 
+    public void deleteBoards(){
 
-        List<WebElement> threeDotsList = Driver.getDriver().findElements(By.xpath("//div[@class='smooth-dnd-container vertical']//div//button[@aria-label='Actions']"));
-        anyThreeDotsIndexNumber = faker.number().numberBetween(0, threeDotsList.size() - 1);
-        threeDotsList.get(anyThreeDotsIndexNumber).click();
-        if (!assignToMe.isDisplayed()){
-
+        List<WebElement> boardsThreeDots = Driver.getDriver().findElements(By.xpath("//ul[@class='app-navigation-entry__children']//button[@aria-label='Actions']"));
+        if (boardsThreeDots.size() != 0){
+            for (WebElement eachBoardsThreeDot : boardsThreeDots) {
+                eachBoardsThreeDot.click();
+                deleteBoard.click();
+            }
         }
 
     }
