160fab5f2e9fbaa01553153ab67dd14c5ab2f2fa
salihabas <104172341+salihabas@users.noreply.github.com>
2022-08-02 12:21:39 +0300

    Update CukesRunner.java

--- src/test/java/net/seamlessly/runners/CukesRunner.java
+++ src/test/java/net/seamlessly/runners/CukesRunner.java
@@ -9,10 +9,10 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {
                 //"pretty",
-                "json:target/cucumber.json",
-                "html:target/cucumber-report.html",
-                "rerun:target/rerun.txt",
-                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+               // "json:target/cucumber.json",
+                "html:target/cucumber-report.html"
+                //"rerun:target/rerun.txt",
+               // "me.jvt.cucumber.report.PrettyReports:target/cucumber",
         },
         features = "src/test/resources/features",
 
